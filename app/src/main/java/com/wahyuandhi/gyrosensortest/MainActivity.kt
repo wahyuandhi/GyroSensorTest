1755450dede85183b838c772152f46a6d961bd91
wahyuandhi <wakay.98@gmail.com>
2021-01-05 11:18:44 +0700

    applying the gyroscope sensor

--- app/src/main/java/com/wahyuandhi/gyrosensortest/MainActivity.kt
+++ app/src/main/java/com/wahyuandhi/gyrosensortest/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.wahyuandhi.gyrosensortest
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wahyuandhi.gyrosensortest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event!!.sensor.type == Sensor.TYPE_GYROSCOPE) {
+            binding.tvGyroX.text = "Sumbu X : "+event.values[0].toString()
+            binding.tvGyroY.text = "Sumbu Y : "+event.values[1].toString()
+            binding.tvGyroZ.text = "Sumbu Z : "+event.values[2].toString()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        // code when accuracy change
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }

4a1838a7fdcf9e8f7020965566f5d1f2e35197e5
wahyuandhi <wakay.98@gmail.com>
2021-01-05 07:16:34 +0700

    Implement Data Binding

--- app/src/main/java/com/wahyuandhi/gyrosensortest/MainActivity.kt
+++ app/src/main/java/com/wahyuandhi/gyrosensortest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.wahyuandhi.gyrosensortest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wahyuandhi.gyrosensortest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
